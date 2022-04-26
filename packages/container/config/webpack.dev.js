e80c1ee0061fdd70c63b861c4d0356b15dc2b208
Tariq Siddiqui <tariq.siddiqui@dastgyr.com>
2022-04-26 15:30:11 +0500

    Init Project

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,25 @@
+const {merge} = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const {dependencies} = require('../package');
+
+const devConfig = {
+    mode: 'development',
+    devServer:{
+        port:8080,
+        historyApiFallback:{
+            index:'index.html'
+        }
+    },
+    plugins:[
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes:{
+                marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+            },
+            shared: dependencies
+        }),
+    ]
+};
+
+module.exports = merge(commonConfig, devConfig);
